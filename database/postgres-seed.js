7c5aab5446b4f60d955da68c176526ba849995a1
jonathanwang0903 <jonathancharles66@gmail.com>
2019-01-11 20:56:39 -0800

    added data generation

--- database/postgres-seed.js
+++ database/postgres-seed.js
@@ -8,9 +8,28 @@ const cn = 'postgresql://jonathan:123@localhost:5432/tracks';
 const db = pgp(cn);
 const cs = new pgp.helpers.ColumnSet(['id', 'artist', 'track', 'album', 'albumart', 'plays', 'likes', 'shares', 'comments'], {table: 'tracks'});
 
-// const getRandomInt = (max) => {
-//   return Math.floor(Math.random() * 800000);
-// };
+const faker = require('faker');
+
+// 10M tracks
+// 50K artists
+// 50k albums
+// 10000 album imgs
+// const tracks = [];
+// const albumArts = [];
+// const artists = [];
+// const albums = [];
+
+// for (var i = 0; i < 10000000; i++) {
+//   if (i < 1000) {
+//     albumArts[i] = faker.random.image();
+//   }
+//   if (i < 50000) {
+//     artists[i] = faker.name.findName();
+//     albums[i] = `${faker.commerce.product()} ${faker.commerce.product()}`
+//   }
+//   tracks[i] = `${faker.lorem.word()} ${faker.lorem.word()}`;
+// }
+
 console.time('seed');
 
 
