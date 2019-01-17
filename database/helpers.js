78089ebdf9e8561ed0dbde5e2872bfb4e074fc3a
jonathanwang0903 <jonathancharles66@gmail.com>
2019-01-16 20:23:31 -0800

    implemented CRUD

--- database/helpers.js
+++ database/helpers.js
@@ -6,15 +6,54 @@ const db = pgp(cn);
 
 
 const getTracks = (callback) => {
-  db.query('SELECT * from tracks where id >= 9990000 and id < 10000000')
+  db.query('SELECT * from tracks where id >= 9900001 and id <= 10000000')
   .then(data => callback(null, data))
   .catch(error => console.log(error) );
 };
 
-const sortTracks = (data, id) => {
-  // console.log(data);
-  // const trackId = id - 1 || 0;
-  const currentTrack = data[id-9990000];
+const getOneTrack = (id, callback) => {
+  db.query(`SELECT * FROM tracks WHERE id = ${id}`)
+  .then(data => callback(null, data))
+  .catch(error => console.log(error));
+};
+
+const createOneTrack = (req, callback) => {
+  db.query('SELECT count(*) AS exact_count FROM tracks')
+  .then(data => data[0].exact_count)
+  .then(id => {
+    db.query(`INSERT INTO tracks VALUES
+      (${Number(id)+1}, ${req.query.artist}, ${req.query.track}, ${req.query.album}, ${req.query.albumart},
+       ${req.query.plays}, ${req.query.likes}, ${req.query.shares}, ${req.query.comments})`)
+    .then(() => callback())
+    .catch(error => console.log(error));
+  });
+};
+
+const updateOneTrack = (req, callback) => {
+  // console.log(req.query)
+  db.query(`UPDATE tracks
+            SET artist = ${req.query.artist},
+              track = ${req.query.track},
+              album = ${req.query.album},
+              albumart = ${req.query.albumart},
+              plays = ${Number(req.query.plays)},
+              likes = ${Number(req.query.likes)},
+              shares = ${Number(req.query.shares)},
+              comments = ${Number(req.query.comments)}
+            WHERE id = ${Number(req.query.id)}`)
+  .then(() => callback())
+  .catch(error => console.log('error'));
+};
+
+const deleteOneTrack = (id, callback) => {
+  db.query(`DELETE FROM tracks WHERE id = ${id}`)
+  .then(() => callback())
+  .catch(error => console.log('error'));
+}
+
+const sortTracks = (data, currentTrack) => {
+  // console.log(currentTrack);
+  currentTrack = currentTrack[0];
   const relatedTracks = [];
   const playlists = [];
   for (let i = 0; i < data.length; i += 1) {
@@ -23,6 +62,7 @@ const sortTracks = (data, id) => {
     } else if (data[i].album === currentTrack.album && relatedTracks.length >= 3
       && playlists.length < 3) {
       playlists.push(data[i]);
+      if (playlists.length === 3) break;
     }
   }
   const filteredData = {
@@ -30,10 +70,15 @@ const sortTracks = (data, id) => {
     relTracks: relatedTracks,
     plists: playlists,
   };
+  // console.log(filteredData);
   return filteredData;
 };
 
 module.exports = {
   getTracks,
+  getOneTrack,
+  createOneTrack,
+  updateOneTrack,
+  deleteOneTrack,
   sortTracks,
 };
--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const async = require('async');
+const Promise = require('bluebird');
 
 const getTracks = require('../database/helpers.js').getTracks;
 const sortTracks = require('../database/helpers.js').sortTracks;
+<<<<<<< Updated upstream
+=======
+const getOneTrack = require('../database/helpers.js').getOneTrack;
+const createOneTrack = require('../database/helpers').createOneTrack;
+const updateOneTrack = require('../database/helpers').updateOneTrack;
+const deleteOneTrack = require('../database/helpers').deleteOneTrack;
+>>>>>>> Stashed changes
 
 const app = express();
 // app.set('port', process.env.PORT || 3001);
@@ -22,20 +30,66 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-
+// console.time('get')
 app.get('/:id', (req, res) => {
   // res.send('hello');
   let id = req.params.id;
-  getTracks((err, data) => {
+  let currentTrack;
+  getOneTrack(id, (err, data) => {
     if (err) return err;
-    const sortedData = sortTracks(data, id);
-    // console.log(sortedData);
-    res.send(sortedData);
+    currentTrack = data;
+  });
+  getTracks((err, data) => {
+    // console.log(currentTrack);
+    if (err) return err;
+    const sortedTracks = sortTracks(data, currentTrack);
+    res.send(sortedTracks);
+    // console.timeEnd('get');
   });
 });
 
-const port = 3001;
+/* CRUD operations */
 
+// Create a new item
+// format:
+// http://localhost:3001/api?artist='artist'&track='track'&album='album'&albumart='albumart'&plays=1&likes=1&shares=1&comments=1
+app.post('/api/track', (req, res) => {
+  createOneTrack(req, () => {
+    res.send('Added one new track to database');
+  });
+});
+
+// Read an item
+app.get('/api/track/:id', (req, res) => {
+  const id = req.params.id;
+  getOneTrack(id, (err, data) => {
+    if (err) return err;
+    res.send(data);
+  });
+});
+
+// Update an item
+// format same as post
+app.put('/api/track', (req, res) => {
+  updateOneTrack(req, () => {
+    res.send('Updated selected track');
+  });
+});
+
+// Delete an item
+app.delete('/api/track/:id', (req, res) => {
+  const id = req.params.id;
+  deleteOneTrack(id, () => {
+    res.send('Deleted selected track');
+  });
+})
+
+/* set up server connection */
+const port = 3001;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
+
+
+
